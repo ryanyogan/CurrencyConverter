2f5adc28fdc88255ef278360620fa4b6772c0a10
Ryan Yogan <ryanyogan@gmail.com>
2018-01-27 11:43:12 -0800

    Added <Logo /> component, images, setup basic <Home /> screen

--- app/index.js
+++ app/index.js
@@ -4,7 +4,8 @@ import ESStyleSheet from 'react-native-extended-stylesheet';
 import Home from './screens/Home';
 
 ESStyleSheet.build({
-  $primaryBlue: '#4F6D7A'
+  $primaryBlue: '#4F6D7A',
+  $white: '#FFFFFF'
 });
 
 export default () => <Home />;
