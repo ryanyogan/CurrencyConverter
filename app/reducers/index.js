8e6a6700e6e1e2ca738dbdf427c509275502015b
Ryan Yogan <ryanyogan@gmail.com>
2018-01-28 19:31:05 -0800

    setup basic redux a / r for currencies

---
+++ app/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+import currencies from './currencies';
+
+export default combineReducers({ currencies });
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -9,6 +9,8 @@ import { ClearButton } from '../components/Buttons';
 import { LastConverted } from '../components/Text';
 import { Header } from '../components/Header';
 
+import { swapCurrency, changeCurrencyAmount } from '../actions/currencies';
+
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
@@ -27,12 +29,12 @@ class Home extends Component {
   handlePressQuoteCurrency = () =>
     this.props.navigation.navigate('CurrencyList', { title: 'Quote Currency' });
 
-  handleTextChange = text => {
-    console.log('change text', text);
+  handleTextChange = amount => {
+    this.props.disaptch(changeCurrencyAmount(amount));
   };
 
   handleSwapCurrency = () => {
-    console.log('press swap currency');
+    this.props.dispatch(swapCurrency());
   };
 
   handleOptionsPress = () => this.props.navigation.navigate('Options');
