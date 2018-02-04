684145f0c4125c6ca2607aec86a02a08f1c254a6
Ryan Yogan <ryanyogan@gmail.com>
2018-02-04 13:26:24 -0800

    Updated to json for eslint, stubbed silly test to make sure Jest is rocking

--- App.test.js
+++ App.test.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import App from './App';
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+it('succeeds', () => {
+  expect(true).toBeTruthy();
 });
