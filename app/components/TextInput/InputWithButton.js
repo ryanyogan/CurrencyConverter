0073d6a4606e83d29ae958310cce89bff3ef2ac4
Ryan Yogan <ryanyogan@gmail.com>
2018-02-04 10:30:33 -0800

    Animate the currencyQuote field when the value has changed

--- app/components/TextInput/InputWithButton.js
+++ app/components/TextInput/InputWithButton.js
@@ -1,51 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { func, string, bool } from 'prop-types';
-import { View, TouchableHighlight, Text, TextInput } from 'react-native';
+import {
+  View,
+  TouchableHighlight,
+  Text,
+  TextInput,
+  Animated,
+} from 'react-native';
 import color from 'color';
 
 import styles from './styles';
 
-const InputWithButton = props => {
-  const { onPress, buttonText, editable = true, textColor } = props;
+class InputWithButton extends Component {
+  static propTypes = {
+    onPress: func,
+    buttonText: string,
+    editable: bool,
+    textColor: string,
+    value: string,
+  };
 
-  const containerStyles = [styles.container];
-  if (editable === false) {
-    containerStyles.push(styles.containerDisabled);
+  constructor(props) {
+    super(props);
+    this.backgroundColor = new Animated.Value(0);
   }
 
-  const buttonTextStyles = [styles.buttonText];
-  if (textColor) {
-    buttonTextStyles.push({ color: textColor });
+  componentDidUpdate() {
+    if (parseFloat(this.props.value)) {
+      Animated.sequence([
+        Animated.timing(this.backgroundColor, {
+          toValue: 1,
+        }),
+        Animated.timing(this.backgroundColor, {
+          toValue: 0,
+        }),
+      ]).start();
+    }
   }
 
-  const underlayColor = color(styles.$buttonBackgroundColorBase).darken(
-    styles.$buttonBackgroundColorModifier,
-  );
+  render() {
+    const { onPress, buttonText, editable = true, textColor } = this.props;
 
-  return (
-    <View style={containerStyles}>
-      <TouchableHighlight
-        underlayColor={underlayColor}
-        style={styles.buttonContainer}
-        onPress={onPress}
-      >
-        <Text style={buttonTextStyles}>{buttonText}</Text>
-      </TouchableHighlight>
-      <View style={styles.border} />
-      <TextInput
-        underlineColorAndroid="transparent"
-        style={styles.input}
-        {...props}
-      />
-    </View>
-  );
-};
+    const containerStyles = [styles.container];
+    if (editable === false) {
+      // containerStyles.push(styles.containerDisabled);
+      containerStyles.push({
+        backgroundColor: this.backgroundColor.interpolate({
+          inputRange: [0, 1],
+          outputRange: [
+            styles.$inputBackgroundBase,
+            styles.$inputBackgroundAlt,
+          ],
+        }),
+      });
+    }
 
-InputWithButton.propTypes = {
-  onPress: func,
-  buttonText: string,
-  editable: bool,
-  textColor: string,
-};
+    const buttonTextStyles = [styles.buttonText];
+    if (textColor) {
+      buttonTextStyles.push({ color: textColor });
+    }
+
+    const underlayColor = color(styles.$buttonBackgroundColorBase).darken(
+      styles.$buttonBackgroundColorModifier,
+    );
+
+    return (
+      <Animated.View style={containerStyles}>
+        <TouchableHighlight
+          underlayColor={underlayColor}
+          style={styles.buttonContainer}
+          onPress={onPress}
+        >
+          <Text style={buttonTextStyles}>{buttonText}</Text>
+        </TouchableHighlight>
+        <View style={styles.border} />
+        <TextInput
+          underlineColorAndroid="transparent"
+          style={styles.input}
+          {...this.props}
+        />
+      </Animated.View>
+    );
+  }
+}
 
 export default InputWithButton;
