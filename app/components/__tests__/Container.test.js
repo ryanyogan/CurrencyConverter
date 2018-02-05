ed0ea873682860f436414961e5ea6b8ff87b3772
Ryan Yogan <ryanyogan@gmail.com>
2018-02-04 17:11:02 -0800

    Implemented basic snapshots (they fucking rock harder on RN than web!)

--- app/components/__tests__/Container.test.js
+++ app/components/__tests__/Container.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { View } from 'react-native';
 
 import { Container, styles } from '../Container';
 
@@ -10,4 +11,22 @@ it('renders the Container component', () => {
 
 it('styles is an object', () => {
   expect(typeof styles).toBe('object');
+});
+
+it('renders the child prop', () => {
+  const rendered = renderer
+    .create(
+      <Container>
+        <View />
+      </Container>,
+    )
+    .toJSON();
+  expect(rendered).toMatchSnapshot();
+});
+
+it('uses the specified backgroundColor, if prop is provided', () => {
+  const rendered = renderer
+    .create(<Container backgroundColor="red" />)
+    .toJSON();
+  expect(rendered).toMatchSnapshot();
 });
