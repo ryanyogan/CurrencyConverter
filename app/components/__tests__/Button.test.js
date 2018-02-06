76dcaf87d39f6f78e896f7a1418aadb6b1959d38
Ryan Yogan <ryanyogan@gmail.com>
2018-02-06 08:49:02 -0800

    Added in enzyme for UI interaction tests, basic button onPress stub

--- app/components/__tests__/Button.test.js
+++ app/components/__tests__/Button.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import { TouchableOpacity } from 'react-native';
+import sinon from 'sinon';
 
 import buildStyles from '../../config/styles';
 import { ClearButton, styles } from '../Buttons';
@@ -20,4 +23,21 @@ it('styles should be an object', () => {
 it('renders custom text passed via props', () => {
   const rendered = renderer.create(<ClearButton text="test" />).toJSON();
   expect(rendered).toMatchSnapshot();
+});
+
+it('handles a press', () => {
+  const callback = sinon.spy();
+  const wrapper = shallow(
+    <ClearButton text="hey this handles a press" onPress={callback} />,
+  );
+
+  expect(wrapper.find(TouchableOpacity).length).toBe(1);
+
+  wrapper
+    .find(TouchableOpacity)
+    .first()
+    .props()
+    .onPress();
+
+  expect(callback.called).toBe(true);
 });
